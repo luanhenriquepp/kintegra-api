6c94e5aee09ef4cfb2bd9702bbad6a08f42dc2ae
zxcvbnm <lhppaixaoo@gmail.com>
2019-10-08 13:37:38 -0300

    Isolando as responsabilidades para a camada de service, para que o código fique mais legível

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Service\UserService;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    private $service;
+
+    public function __construct(UserService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function login(Request $request)
+    {
+        return $this->service->login($request);
+    }
+}
---
+++ app/Service/UserService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Service;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class UserService
+{
+    protected $user;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+
+    public function login(Request $request) {
+
+        $credentials = $request->only('email', 'password');
+
+        if (!$token = auth('api')->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return response()->json([
+            'token' => $token,
+            'type' => 'bearer',
+            'expires' => auth('api')->factory()->getTTL() * 60,
+
+        ]);
+    }
+}
